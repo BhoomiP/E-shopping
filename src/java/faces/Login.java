7474386ac3d80455bc4815843132f809cee53f8b
BhoomiP <bhoomi.p2910@gmail.com>
2018-04-04 13:34:34 -0700

    commit this

---
+++ src/java/faces/Login.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faces;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author c0690651
+ */
+@ManagedBean
+@SessionScoped
+public class Login {
+
+    private String email;
+    private String password;
+    private boolean loggedIn;
+    private user currentUser;
+    private userController users;
+
+    /**
+     * Default constructor
+     */
+    public Login(){
+        email = null;
+        password = null;
+        loggedIn = false;
+        currentUser = new user();
+        users = new userController();
+    }
+    
+    public String login() {
+        String nextPage = "LogIn";
+        for(user u : users.getUsers()){
+            if((email.equals(u.getEmail()) || email.equals(u.getUser_name())) && password.equals(u.getPassword())){
+                loggedIn = true;
+                currentUser = u;
+                nextPage = "index";
+            }
+        }
+        return nextPage;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public user getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(user currentUser) {
+        this.currentUser = currentUser;
+    }
+}
---
+++ src/java/faces/items.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faces;
+
+/**
+ *
+ * @author c0690651
+ */
+public class items {
+
+   private int item_id;
+   private String item_name;
+   private String description;
+   private int item_price;
+   private int user_id;
+   
+   
+     /**
+     * Default constructor
+     */
+    public items() {
+
+    }
+
+    public items(int item_id, String item_name, String description, int item_price, int user_id) {
+        this.item_id = item_id;
+        this.item_name = item_name;
+        this.description = description;
+        this.item_price = item_price;
+        this.user_id = user_id;
+    }
+    
+    
+
+    public int getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(int item_id) {
+        this.item_id = item_id;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getItem_price() {
+        return item_price;
+    }
+
+    public void setItem_price(int item_price) {
+        this.item_price = item_price;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+  
+
+    
+
+}
